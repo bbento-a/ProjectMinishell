0f2b0ba57c943c672e2b45f1c22ee55aa0d3c3d8
bbento-a <bbento-a@student.42lisboa.com>
2025-01-27 11:40:17 +0000

    Fixed get_env() condition

--- src/miscellaneous/get_env.c
+++ src/miscellaneous/get_env.c
@@ -10,12 +10,16 @@ char	*get_env(char *var)
 	i = 0;
 	while(envp[i])
 	{
-		if (ft_strncmp(envp[i], var, ft_strlen(var)) == 0)
+		// printf("envp[i]: %s\n", envp[i]);
+		if (ft_strncmp(envp[i], var, ft_strlen(var)) == 0) // envp var name needs a size cmp before this condition
 		{
 			tmp = ft_strchr(envp[i], '=');
-			if (!tmp)
+			// printf("tmp: %s\n", tmp);
+			// printf("tmp: %ld; size of tmp: %ld\n", ft_strlen(envp[i]), ft_strlen(tmp));
+			// printf("tmp - size of tmp: %ld\n", ft_strlen(envp[i]) - ft_strlen(tmp));
+			if (!tmp || ft_strlen(envp[i]) - ft_strlen(tmp) != ft_strlen(var))
 				break ;
-			return (tmp);
+			return (tmp++);
 		}
 		i++;
 	}
