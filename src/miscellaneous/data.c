16380c8901260e44d5361cbc18201f280df4644b
bbento-a <bbento-a@student.42lisboa.com>
2025-03-15 08:12:20 +0000

    Norm data

--- src/miscellaneous/data.c
+++ src/miscellaneous/data.c
@@ -6,35 +6,35 @@
 /*   By: bbento-a <bbento-a@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 06:02:15 by mde-maga          #+#    #+#             */
-/*   Updated: 2025/03/15 08:04:12 by bbento-a         ###   ########.fr       */
+/*   Updated: 2025/03/15 08:10:47 by bbento-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-t_env *parse_envp(char **envp)
+t_env	*parse_envp(char **envp)
 {
-    t_env *new_env;
-    t_env *tmp;
-    t_env *last_node;
-    int i = 0;
+	t_env	*new_env;
+	t_env	*tmp;
+	t_env	*last_node;
+	int		i;
 
-    while (envp[i])
-    {
-        tmp = my_malloc(sizeof(t_env));
-        tmp->value = ft_strdup(envp[i]);
-        tmp->envp = &envp[i];
+	i = 0;
+	while (envp[i])
+	{
+		tmp = my_malloc(sizeof(t_env));
+		tmp->value = ft_strdup(envp[i]);
+		tmp->envp = &envp[i];
 		tmp->next = NULL;
 		if (i == 0)
 			new_env = tmp;
 		else
 			last_node->next = tmp;
 		last_node = tmp;
-        i++;
-    }
+		i++;
+	}
 	return (new_env);
 }
-
 
 // Initiates the variables so we're not working wiith uninitialized values
 
